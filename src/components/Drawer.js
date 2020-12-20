953d014e7f4c1a7322a3cacb0c3fdcd5cc590ce3
tdeng3 <tdeng3@hawk.iit.edu>
2020-12-20 02:52:34 -0600

    Added NavBar/Drawer Components

---
+++ src/components/Drawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const DrawerData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Login',
+        path: '/login',
+        icon: <IoIcons.IoIosLogIn />,
+        cName: 'nav-text'
+    },
+]
---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import { NavLink } from "react-router-dom";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import { DrawerData } from './Drawer';
+import './NavBar.css';
+
+function NavBar() {
+    const [drawer, setDrawer] = useState(false);
+    const showDrawer = () => setDrawer(!drawer);
+    return ( 
+        <>
+            <div className='navbar'>
+                <NavLink to='#' className='menu-bars'>
+                    <FaIcons.FaBars onClick = {showDrawer} />
+                </NavLink>
+            </div>
+            <nav className = {drawer ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items' onClick = {showDrawer}>
+                    <li className= 'navbar-toggle'>
+                        <NavLink to='#' className = 'menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </NavLink>
+                    </li>
+                    {DrawerData.map((item, index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <NavLink to= {item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
