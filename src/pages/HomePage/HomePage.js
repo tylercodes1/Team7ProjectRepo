b1a5363677d419105278731f3f03858933d2d4ec
tkmoney11 <tylerkim@outlook.com>
2020-12-22 23:00:15 -0800

    Ignore App.css+Index.css. To revert in final merge

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./HomePage.css";
+import HomePageWelcome from "./../../components/HomePageWelcome/HomePageWelcome";
 export default class HomePage extends React.Component {
-    render() {
-        return (
-            <h1>This is home page</h1>
-        )
-    }
+  render() {
+    return (
+      <div className="home-page">
+        <HomePageWelcome></HomePageWelcome>
+      </div>
+    );
+  }
 }
