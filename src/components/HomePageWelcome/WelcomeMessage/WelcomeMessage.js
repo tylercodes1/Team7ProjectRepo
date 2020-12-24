cd6f7c048595b5f04cc996e2e09a1f312636121d
tdeng3 <tdeng3@hawk.iit.edu>
2020-12-23 22:54:35 -0600

    Added Redux with a clickable test on Login page

--- src/components/HomePageWelcome/WelcomeMessage/WelcomeMessage.js
+++ src/components/HomePageWelcome/WelcomeMessage/WelcomeMessage.js
@@ -2,26 +2,16 @@ import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import "./WelcomeMessage.css";
-import {useSelector, useDispatch} from 'react-redux';
-import {login} from '../../../store/actions/actionTypes'
 
 export default function WelcomeMessage() {
 
-  // test for redux
-  const isLogged = useSelector (state => state.isLogged);
-  const dispatch = useDispatch();
-  // test for redux 
-
-
   return (
     <div className="welcome-message">
-      {isLogged ? <h1>You have loged in.</h1> : <p className="message">Let’s figure out where to eat!</p>}
+      <p className="message">Let’s figure out where to eat!</p>
       <button
         className="welcome-message-button"
-        // onClick={() => console.log("route to screen")}
-        // test for redux
-        onClick = {() => dispatch(login())}
-      >
+        onClick={() => console.log("route to screen")}
+        >
         <IconContext.Provider
           value={{
             size: "35px",
