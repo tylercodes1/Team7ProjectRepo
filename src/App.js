91fd79f1f14abaea761ba7684d57c318696223a2
tkmoney11 <tylerkim@outlook.com>
2020-12-25 17:38:53 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./pages/HomePage/HomePage";
+import MotionCreationPage from "./pages/MotionCreationPage/MotionCreationPage";
 
 function App() {
   const [background, setBackground] = useState(false);
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/motion-creation" component={MotionCreationPage} />
           <Route path="/" render={() => <div>404 NOT FOUND :)</div>} />
         </Switch>
       </Router>
