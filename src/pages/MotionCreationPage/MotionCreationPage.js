497e42ba41f64fd4e119c17f1460a3d18f82f283
tkmoney11 <tylerkim@outlook.com>
2020-12-25 17:39:31 -0800

    Initialize Motion Creation

---
+++ src/pages/MotionCreationPage/MotionCreationPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./MotionCreationPage.css";
+
+export default function MotionCreationPage() {
+  return <div className="motion-creation-page">MotionCreationPage</div>;
+}
