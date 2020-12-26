3e5769392eeee65b3a88bba5806338929f004667
tkmoney11 <tylerkim@outlook.com>
2020-12-25 17:43:13 -0800

    Removed improperly named files

--- src/pages/CreateAMotion/CreateAMotionPage.js
+++
@@ -1,8 +0,0 @@
-// Make a controller-view and view for this page.
-// Selecting restaurants and inviting friends should all be handled
-// in one file, not two separate files.
-import { React } from "react";
-
-export default function CreateAMotionPage() {
-  return;
-}
