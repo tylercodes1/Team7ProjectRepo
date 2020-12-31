319a1e9fb1a6d56a7c7a0d1c2b8b85b2759eb983
tkmoney11 <tylerkim@outlook.com>
2020-12-31 12:00:09 -0800

    Added path to Motion Creation

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
 export const DrawerData = [
-    {
-        title: 'Home',
-        path: '/',
-        icon: <AiIcons.AiFillHome />,
-        cName: 'nav-text'
-    },
-    {
-        title: 'Login',
-        path: '/login',
-        icon: <IoIcons.IoIosLogIn />,
-        cName: 'nav-text'
-    },
-]
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Login",
+    path: "/login",
+    icon: <IoIcons.IoIosLogIn />,
+    cName: "nav-text",
+  },
+  {
+    title: "MotionCreation",
+    path: "motion-creation",
+    icon: <AiIcons.AiFillPlusCircle />,
+    cName: "nav-text",
+  },
+];
