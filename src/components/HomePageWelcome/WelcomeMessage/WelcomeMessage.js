4654663e0b5257cdab25ad4a071c4cb3f26f07ea
Macy M <mei.mcanally@ttu.edu>
2021-01-01 23:59:17 -0600

    linked the motion creation page to the homepage by clicking the + button

--- src/components/HomePageWelcome/WelcomeMessage/WelcomeMessage.js
+++ src/components/HomePageWelcome/WelcomeMessage/WelcomeMessage.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import "./WelcomeMessage.css";
+import { useHistory } from "react-router-dom";
 
 export default function WelcomeMessage() {
 
+  const history = useHistory();
   return (
     <div className="welcome-message">
       <p className="message">Let’s figure out where to eat!</p>
       <button
         className="welcome-message-button"
-        onClick={() => console.log("route to screen")}
+        onClick={() => history.push('/motion-creation')}
         >
         <IconContext.Provider
           value={{
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,34 @@
-import React from "react";
+// import React from "react";
+// import {useHistory} from "react-router-dom";
+// import "./HomePage.css";
+// import HomePageWelcome from "./../../components/HomePageWelcome/HomePageWelcome";
+// export default class HomePage extends React.Component {
+
+//  //history = useHistory();
+
+//   render() {
+//     return (
+//       <>
+//         <div className="home-page">
+//           <HomePageWelcome />
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+import React from 'react'
 import "./HomePage.css";
 import HomePageWelcome from "./../../components/HomePageWelcome/HomePageWelcome";
-export default class HomePage extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="home-page">
-          <HomePageWelcome></HomePageWelcome>
-        </div>
-      </>
-    );
-  }
+
+function HomePage() {
+
+
+  return (
+    <div className="home-page">
+      <HomePageWelcome />
+    </div>
+  )
 }
+
+export default HomePage
